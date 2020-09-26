ef7ea0e0dd366ee536c251af3335b560a1ad04d2
matias pizarro <matias@pizarro.net>
2020-09-26 04:04:50 +0200

    Beyond Compare as a Linux app

---
+++ editors/linux-bcompare/files/nosched.c
@@ -0,0 +1,32 @@
+#define _GNU_SOURCE
+
+#include <dlfcn.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+
+int pthread_noop() {
+  fprintf(stderr, "%s(...)\n", __func__);
+  return 0;
+}
+
+// https://stackoverflow.com/questions/15599026/how-can-i-intercept-dlsym-calls-using-ld-preload/18825060#18825060
+void* _dl_sym(void*, const char*, void*);
+
+static void* (*libc_dlsym)(void*, const char*) = NULL;
+
+void* dlsym(void* handle, const char* symbol) {
+
+  if (!libc_dlsym) {
+    libc_dlsym = _dl_sym(RTLD_NEXT, "dlsym", dlsym);
+  }
+
+  //~ fprintf(stderr, "%s(_, %s)\n", __func__, symbol);
+
+  if (strcmp(symbol, "pthread_attr_setinheritsched") == 0) {
+    return pthread_noop;
+  }
+
+  return libc_dlsym(handle, symbol);
+}
+
