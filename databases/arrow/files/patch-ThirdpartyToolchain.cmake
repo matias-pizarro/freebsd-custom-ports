3323d1edecdb15b378421365f2df3dcbad81322a
matias pizarro <matias@pizarro.net>
2020-09-29 16:28:09 +0200

    Add S3 build option

--- databases/arrow/files/patch-ThirdpartyToolchain.cmake
+++ databases/arrow/files/patch-ThirdpartyToolchain.cmake
@@ -1,6 +1,14 @@
 --- cmake_modules/ThirdpartyToolchain.cmake.orig	2020-08-17 17:58:35 UTC
 +++ cmake_modules/ThirdpartyToolchain.cmake
-@@ -2182,10 +2182,10 @@
+@@ -16,6 +16,7 @@
+ # under the License.
+ 
+ include(ProcessorCount)
++include(ExternalProject)
+ processorcount(NPROC)
+ 
+ add_custom_target(rapidjson)
+@@ -2182,10 +2183,10 @@
    file(MAKE_DIRECTORY ${CARES_INCLUDE_DIR})
  
    add_dependencies(toolchain cares_ep)
@@ -15,7 +23,7 @@
  
    set(CARES_VENDORED TRUE)
  
-@@ -2194,7 +2194,7 @@
+@@ -2194,7 +2195,7 @@
  
  if(ARROW_WITH_GRPC)
    if(c-ares_SOURCE STREQUAL "AUTO")
@@ -24,7 +32,7 @@
      if(c-ares_FOUND)
        set(CARES_INCLUDE_DIR ${c-ares_INCLUDE_DIR})
      else()
-@@ -2203,7 +2203,7 @@
+@@ -2203,7 +2204,7 @@
    elseif(c-ares_SOURCE STREQUAL "BUNDLED")
      build_cares()
    elseif(c-ares_SOURCE STREQUAL "SYSTEM")
@@ -33,3 +41,11 @@
      set(CARES_INCLUDE_DIR ${c-ares_INCLUDE_DIR})
    endif()
  
+@@ -2658,6 +2659,7 @@
+ if(ARROW_S3)
+   # See https://aws.amazon.com/blogs/developer/developer-experience-of-the-aws-sdk-for-c-now-simplified-by-cmake/
+ 
++  set(BUILD_SHARED_LIBS ON)
+   # Need to customize the find_package() call, so cannot call resolve_dependency()
+   if(AWSSDK_SOURCE STREQUAL "AUTO")
+     find_package(AWSSDK COMPONENTS config s3 transfer)
