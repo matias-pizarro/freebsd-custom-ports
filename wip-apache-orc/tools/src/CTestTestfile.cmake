167571e13888f22ef3bdb233b4897dea75ef0e08
matias pizarro <matias@pizarro.net>
2021-05-22 16:23:53 +0200

    Remove obsolete WiP port

--- wip-apache-orc/tools/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/home/mpizarro/devel/ports/databases/apache-orc/work/orc-rel-release-1.6.4/tools/src
-# Build directory: /usr/home/mpizarro/devel/ports/databases/apache-orc/tools/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
